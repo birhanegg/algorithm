59f0bce37da211d494781801c47493530767c398
GirmaTeklemariam <59131108+GirmaTeklemariam@users.noreply.github.com>
2020-02-27 18:19:50 -0600

    Lab2_Question#4

---
+++ AlgorithmLab/src/PowerSet/PowerSet.java
@@ -0,0 +1,44 @@
+package PowerSet;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class PowerSet {
+	
+	public static <T> List<Set<T>> powerSet(List<T> X){
+		List<Set<T>> P = new ArrayList<Set<T>>();
+		Set<T> S = new HashSet<T>();
+		P.add(S);
+		if(X.isEmpty()) {
+			return P;
+		}
+		else {
+			while(!X.isEmpty()) {
+				T f = X.remove(0);
+				List<Set<T>> temp = new ArrayList<Set<T>>();
+				for(Set<T> x : P) {		//to void ConcurrentModification Exception	
+					temp.add(x);
+				}
+				for(Set<T> x : temp) {
+					S = new HashSet<T>();
+					S.add(f);
+					S.addAll(x);
+					P.add(S);					
+				}					
+			}			
+		}
+		return P;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(4);
+		list.add(5);
+		System.out.println(PowerSet.powerSet(list));
+	}
+
+}
